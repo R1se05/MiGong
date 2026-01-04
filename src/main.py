92fbb064be93bec5ca33879cd61f4ae6d452cc42
R1se05 <3362967790@qq.com>
2026-01-04 13:58:05 +0800

    添加ASCII显示

--- src/main.py
+++ src/main.py
@@ -25,9 +25,34 @@ class Maze:
             else:
                 self.walls[y1][x1][3] = False
                 self.walls[y2][x2][1] = False
+    
+    def print_maze_ascii(self):  # 新增方法
+        print("+" + "---+" * self.width)
+        for y in range(self.height):
+            row_str = "|"
+            bottom_str = "+"
+            for x in range(self.width):
+                cell_content = " "
+                if (y, x) == self.start:
+                    cell_content = "S"
+                elif (y, x) == self.end:
+                    cell_content = "E"
+                
+                if self.walls[y][x][1]:
+                    row_str += f" {cell_content} |"
+                else:
+                    row_str += f" {cell_content}  "
+                
+                if self.walls[y][x][2]:
+                    bottom_str += "---"
+                else:
+                    bottom_str += "   "
+                bottom_str += "+"
+            print(row_str)
+            print(bottom_str)
 
 
-class MazeGenerator:  # 新增类
+class MazeGenerator:
     @staticmethod
     def generate_dfs(maze):
         sys.setrecursionlimit(maze.width * maze.height * 2)
@@ -49,14 +74,14 @@ class MazeGenerator:  # 新增类
 
 
 def main():
-    print("=== 迷宫生成系统 v2.0 ===")  # 修改版本号
-    print("添加DFS生成算法")  # 修改功能描述
+    print("=== 迷宫生成系统 v3.0 ===")  # 修改版本号
+    print("添加ASCII显示功能")  # 修改功能描述
     
-    maze = Maze(5, 5)
-    MazeGenerator.generate_dfs(maze)  # 调用生成算法
+    maze = Maze(8, 8)  # 修改：改为8x8
+    MazeGenerator.generate_dfs(maze)
     
-    print(f"创建了 {maze.width}x{maze.height} 的迷宫")
-    print("使用DFS算法生成了迷宫")
+    print(f"\n{8}x{8} 迷宫：")  # 修改输出
+    maze.print_maze_ascii()  # 调用显示方法
 
 
 if __name__ == "__main__":
